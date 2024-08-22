aa42a56a6e8b43ad2d78fd3b958d8df50b241963
Miki <bada.bing.bing@live.com>
2024-08-22 08:32:00 +0200

    ✨ [raycast] Add cold-turkey start-work-session script

---
+++ raycast/cold-turkey-start-work-session.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title start-work-session
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🦃
+# @raycast.packageName cold-turkey
+
+# https://getcoldturkey.com/support/user-guide/#part-commands
+"/Applications/Cold Turkey Blocker.app/Contents/MacOS/Cold Turkey Blocker" -start work_session
