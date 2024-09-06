370ed3653226604146bbd9ddfe4c9d7e5540efcd
Miki <bada.bing.bing@live.com>
2024-09-06 07:46:50 +0200

    ✨ [raycast] Add set-default-statusbar

---
+++ raycast/sketchybar-default.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title set-default-statusbar
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🖥️
+# @raycast.packageName sketchybar
+
+sketchybar --reload sketchybarrc
