bec6e3b962184fc91341b333b44d807be6b5cc71
Miki <bada.bing.bing@live.com>
2025-03-28 12:59:16 +0100

    ✨ [dev_env] Add scripts to bootstrap new tmux work sessions

---
+++ local_development/tmux.bootstrap.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+SESSION_NAME=$1
+
+# Check if session-specific bootstrap script exists in the tmux directory
+SCRIPT_PATH="$HOME/src/scripts/local_development/tmux/$SESSION_NAME.tmux.sh"
+SESSION_PATH="$HOME/src/$SESSION_NAME"
+
+
+cd $SESSION_PATH
+
+# If there is a custom script use it too bootstrap the session, otherwise use the default approach
+if [ -f "$SCRIPT_PATH" ]; then
+    source "$SCRIPT_PATH"
+    exit 0
+else
+    tmux rename-window -t $SESSION_NAME:1 "run" # rename the first window
+    if [ -f "$HOME/Library/CloudStorage/Dropbox/toolbox/env/mprocs/$SESSION_NAME.yaml" ]; then
+      tmux send-keys -t $SESSION_NAME:run "mprocs -c /Users/miki/Library/CloudStorage/Dropbox/toolbox/env/mprocs/$SESSION_NAME.yaml" C-m
+    fi
+    tmux new-window -t $SESSION_NAME -n "edit"
+fi
---
+++ local_development/tmux_sessionizer.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# TODO: check primagen's script, it is simpler and more elegant
+
+# TODO change structure of projects directories - raycast extension and go applications are not direct children of ~/src
+# Consider to reorganize the projects in a more structured way (not necessarily to update the script)
+
+# 0. Select session name
+# - Get first level directories from the ~/src directory and select one
+session=$(find ~/src -maxdepth 1 -mindepth 1 -type d | fzf)
+session_name=$(basename $session | tr . -)
+
+if [[ -z "$session_name" ]]; then
+    exit 1
+fi
+
+# ACTIVE_PROJECT is used for sketchybar and raycast extension (not perfect but good enough solution)
+sed -i -e "s/^ACTIVE_PROJECT=.*/ACTIVE_PROJECT=$session_name/" ~/src/raycast-extensions/wa-2/.env
+
+# 1. Setup a new tmux session with the selected name
+# - Only create the session if it doesn't exist already
+# [tmux caveat] has-session -t '<session_name>' will return true even if it finds '<session_name>-extra-words'
+# - for exact lookup you need to use -t='<session_name>' (in that case it will not return true for '<session_name>-extra-words')
+if ! tmux has-session -t=$session_name 2>/dev/null; then
+    tmux new-session -d -s $session_name -c $session
+    sh ~/src/scripts/local_development/tmux.bootstrap.sh $session_name
+fi
+
+# 3. Attach to the selected session
+if [ -z "$TMUX" ]; then
+    # if not inside a tmux session
+    tmux attach-session -t $session_name
+else
+     # if already inside a tmux session
+    tmux switch-client -t $session_name
+fi
+
+# 1. Run tmux sessionizer
+# 2. determine if session exists
+# / if session doesnt exist
+# A.1 create session (detached)
+# A.2 bootstrap it (includes part covered by hydration)
+# A.3 switch (attach) to it
+# / if session exists
+# B.1 (optional) hydrate the session
+# B.2 switch to it
+
+# bootstrap
+# if there is a session specific config, bootstrap using it
+# if there is no specific config, use default
+
+# config
+# session specific config (e.g., cart ui)
+# 1.) send keys: start wa-2 - preprare working environment - print output in "edit window"
+# 2.) start mprocs - prepare local development setup 
+# 3.) (optional) - utils: tests, diagnostics, jenkins pipeline (could also be part of mprocs, but without autostart)
+
+# layout (chould apply to all sessions)
+# 2 windows: run and edit
+# run for "run servers and commands"
+# edit for "make changes": editor and lazygit and others?
+
+# what about slumber and yazi?
+
+# destructing a session should destroy the whole environment created by the wa2
