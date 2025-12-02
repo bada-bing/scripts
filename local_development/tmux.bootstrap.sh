04b511886d059b09aa7eb1fe79e35d76df499d71
Miki <bada.bing.bing@live.com>
2025-12-02 19:57:33 +0100

    ♻️ [local_dev] Update tmux sessions bootstrap script - Extract LOCAL_ENV variable and update path references in tmux bootstrap

--- local_development/tmux.bootstrap.sh
+++ local_development/tmux.bootstrap.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 SESSION_NAME=$1
+LOCAL_ENV="$HOME/Documents/toolbox/env"
 
-# Check if session-specific bootstrap script exists in the tmux directory
-SCRIPT_PATH="$HOME/src/scripts/local_development/tmux/$SESSION_NAME.tmux.sh"
-SESSION_PATH="$HOME/src/$SESSION_NAME"
+# Check if session-specific bootstrap script exists in the local env tmux directory
+SCRIPT_PATH="$LOCAL_ENV/tmux/$SESSION_NAME.tmux.sh"
+PROJECT="$HOME/src/$SESSION_NAME"
 
-
-cd $SESSION_PATH
+cd $PROJECT
 
 # If there is a custom script use it too bootstrap the session, otherwise use the default approach
 if [ -f "$SCRIPT_PATH" ]; then
@@ -15,8 +15,8 @@ if [ -f "$SCRIPT_PATH" ]; then
     exit 0
 else
     tmux rename-window -t $SESSION_NAME:1 "run" # rename the first window
-    if [ -f "$HOME/Documents/toolbox/env/mprocs/$SESSION_NAME.yaml" ]; then
-      tmux send-keys -t $SESSION_NAME:run "mprocs -c /Users/miki/Documents/toolbox/env/mprocs/$SESSION_NAME.yaml" C-m
+    if [ -f "$LOCAL_ENV/mprocs/$SESSION_NAME.yaml" ]; then
+      tmux send-keys -t $SESSION_NAME:run "mprocs -c $LOCAL_ENV/mprocs/$SESSION_NAME.yaml" C-m
     fi
     tmux new-window -t $SESSION_NAME -n "edit" "sh -c '/opt/homebrew/bin/onefetch; exec $SHELL -l'"
 fi
