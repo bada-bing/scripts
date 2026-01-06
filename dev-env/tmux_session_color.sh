6afe3ab8b09a76467af9520deec7a51f9f13fefc
Miki <bada.bing.bing@live.com>
2026-01-06 16:48:32 +0100

    🚛 [dev-env] Move script to color tmux status-bar
    Move from to dev-env directory

--- dev-env/tmux_session_color.sh
+++ dev-env/tmux_session_color.sh
@@ -10,21 +10,21 @@ color=$((16#${hash:0:6} % 216 + 16))
 if [ "$side" = "right" ]; then
     # Right side - output the color code, with padding to extend the colored background and tmux will add the text
     # Outputs: 50 spaces with the colored background
-    printf "#[bg=colour%d,fg=colour0]%-50s" "$color" " "
+    # printf "#[bg=colour%d,fg=colour0]%-50s" "$color" " "
+    printf "#[bg=colour%d,fg=colour0]" "$color"
 else
     # Left side - session name with icon
     session_upper=$(echo "$session_name" | tr '[:lower:]' '[:upper:]')
     printf "#[bg=colour%d,bold,fg=colour0]%-3s${session_upper}%-3s❯ #[nobold]" "$color"
+
+    # Convert xterm-256 color to hex (and print them in the statusbar)
+    # For colors 16-231 (216-color cube)
+
+    if [ "$color" -ge 16 ] && [ "$color" -le 231 ]; then
+        idx=$((color - 16))
+        r=$(( (idx / 36) * 51 ))
+        g=$(( ((idx % 36) / 6) * 51 ))
+        b=$(( (idx % 6) * 51 ))
+        printf "#%02x%02x%02x\n" "$r" "$g" "$b"
+    fi
 fi
-
-
-# Convert xterm-256 color to hex (and print them in the statusbar)
-# For colors 16-231 (216-color cube)
-
-# if [ "$color" -ge 16 ] && [ "$color" -le 231 ]; then
-#     idx=$((color - 16))
-#     r=$(( (idx / 36) * 51 ))
-#     g=$(( ((idx % 36) / 6) * 51 ))
-#     b=$(( (idx % 6) * 51 ))
-#     printf "#%02x%02x%02x\n" "$r" "$g" "$b"
-# fi
--- local_development/tmux.bootstrap.sh
+++ local_development/tmux.bootstrap.sh
@@ -19,4 +19,5 @@ else
       tmux send-keys -t $SESSION_NAME:run "mprocs -c $LOCAL_ENV/mprocs/$SESSION_NAME.yaml" C-m
     fi
     tmux new-window -t $SESSION_NAME -n "edit" "sh -c '/opt/homebrew/bin/onefetch; exec $SHELL -l'"
+    tmux new-window -t $SESSION_NAME -n "gemini"
 fi
